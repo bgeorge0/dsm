1a6e9075c61e4104a68c15262f68293d74f3aca9
Ben George <ben.george@oncology.ox.ac.uk>
2019-06-05 14:02:25 +0100

    Legacy veresion

--- bg_unwrapping/src/from_np.m
+++
@@ -1,8 +0,0 @@
-function C = from_np(A)
-
-shape = cellfun(@int64,cell(A.shape));
-ls = py.array.array('d',A.flatten('F').tolist());
-p = double(ls);
-C = reshape(p,shape);
-
-end
--- bg_unwrapping/src/shortest_as_points.m
+++
@@ -1,9 +0,0 @@
-function UNWRAPPING_DATA = shortest_as_points(UNWRAPPING_DATA)
-
-xx = UNWRAPPING_DATA.as_voxels.gridCOx(UNWRAPPING_DATA.as_voxels.SHORTEST_PATH(:,1));
-yy = UNWRAPPING_DATA.as_voxels.gridCOy(UNWRAPPING_DATA.as_voxels.SHORTEST_PATH(:,2));
-zz = UNWRAPPING_DATA.as_voxels.gridCOz(UNWRAPPING_DATA.as_voxels.SHORTEST_PATH(:,3));
-
-UNWRAPPING_DATA.as_points.shortest_path = [xx' yy' zz'];
-
-end
